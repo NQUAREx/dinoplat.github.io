bf40eb8dfdf1c4ec11fe196ab89f9afaa58f834c
NQUARE <100482393+NQUAREx@users.noreply.github.com>
2026-03-24 13:01:28 +0300

    Remove Telegram bot and telemetry mechanics

--- telemetry.config.js
+++
@@ -1,5 +0,0 @@
-window.DinoPlatTelemetryConfig = {
-  enabled: true,
-  telegramEndpoint: "https://api.telegram.org/bot8767577727:AAHXo9WI1dDMyq0k3aRaJLQFJrXQ73vX-sw/sendMessage?chat_id=8370650143",
-  heartbeatMs: 60000
-};
